ab75a23b6f6cf8ccb8a015953ec3a5d34d6058e4
JayaSri01 <52323441+JayaSri01@users.noreply.github.com>
2023-11-21 21:57:09 +0530

    Update ZipcodeApplicationTests.java

--- Zipcode/src/test/java/com/example/Zipcode/SpringAss03ApplicationTests.java
+++ Zipcode/src/test/java/com/example/Zipcode/SpringAss03ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringAss03ApplicationTests {
+class ZipcodeApplicationTests {
 
 	@Test
 	void contextLoads() {
